8398b06663141693edbe30f853c5f5cf5e8342ff
gdh <gdh@163.com>
2022-04-01 11:16:02 +0800

    first commit

---
+++ src/main/java/com/example/boot/demo/controller/HelloWorldController.java
@@ -0,0 +1,24 @@
+package com.example.boot.demo.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
+
+@RestController
+public class HelloWorldController {
+
+    @Value("${server.port}")
+    private String port;
+    @RequestMapping("/hello")
+    public String index() {
+        ConcurrentHashMap m =new ConcurrentHashMap();
+        AtomicInteger i;
+
+        AtomicReference<String> a;
+        return port;
+    }
+}
